3267152ac9e586b4da0d659bffa10a31327fca55
510599228@qq.com <510599228@qq.com>
2020-01-20 22:40:49 -0800

    add2

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function setup() {
   fontSize = screenPct * 150;
   INNER_RADIUS = screenPct * 240;
   RADIUS_VARIATION = screenPct * 240;
-  
+	
   textFont('Helvetica');
   textSize(fontSize);
 }
@@ -51,23 +51,23 @@ function draw() {
       colors.push(c);
     }
   }
-  // console.log(colors[0]._array)
-  colors_sorted_on_red = colors.slice().sort(function(a, b){
+	// console.log(colors[0]._array)
+	colors_sorted_on_red = colors.slice().sort(function(a, b){
   if (a._array[0]>b._array[0]){ return -1;}
   else if (a._array[0]<b._array[0]){ return 1;}
-  else {
+	else {
   return 0;
-  }
+	}
   });
-  
-  //console.log(colors_sorted_on_red[0]._array)
+	
+	//console.log(colors_sorted_on_red[0]._array)
   var i = 0;
   for (var gridY = 0; gridY < tileCount; gridY++) {
     for (var gridX = 0; gridX < tileCount; gridX++) {
       if (mouseIsPressed){
       fill(colors_sorted_on_red[i]);}
-      else{
-        fill(colors[i]);}
+			else{
+				fill(colors[i]);}
       rect(gridX * rectSize, gridY * rectSize, rectSize, rectSize);
       i++;
     }
@@ -87,40 +87,40 @@ function draw() {
   text(s, 52,125, 250, 300);
   
 
-    //draw sphere
-  beginShape();
-    for (let i = 0; i < SEGMENTS; i++) {
-      let p0 = pointForIndex(i/SEGMENTS);
-      vertex(p0.x, p0.y);
-    }
-  endShape(CLOSE);
-  
-  //draw text
-  let pct = atan2(mouseY - centerY, mouseX - centerX) / TWO_PI;//follow mouse
-  //let pct = 0;//dont follow mouse
-  let pixToAngularPct = 2/((INNER_RADIUS+RADIUS_VARIATION/2)*TWO_PI);
-  for (var i = 0; i < textToWrite.length; i++) {
-    let charWidth = textWidth(textToWrite.charAt(i));
-    pct += charWidth/2 * pixToAngularPct;
-    
-    //calculate angle
-    let leftP = pointForIndex(pct-0.01);
-    let rightP = pointForIndex(pct+0.01);
-    let angle = atan2(leftP.y - rightP.y, leftP.x - rightP.x) + PI;
-    
-    push();
-      let p = pointForIndex(pct);
-      //apply angle
-      translate(p.x, p.y);
-      rotate(angle);
-      translate(-p.x, -p.y);
-        fill(0,0,0);
+  	//draw sphere
+	beginShape();
+		for (let i = 0; i < SEGMENTS; i++) {
+			let p0 = pointForIndex(i/SEGMENTS);
+			vertex(p0.x, p0.y);
+		}
+	endShape(CLOSE);
+	
+	//draw text
+	let pct = atan2(mouseY - centerY, mouseX - centerX) / TWO_PI;//follow mouse
+	//let pct = 0;//dont follow mouse
+	let pixToAngularPct = 2/((INNER_RADIUS+RADIUS_VARIATION/2)*TWO_PI);
+	for (var i = 0; i < textToWrite.length; i++) {
+		let charWidth = textWidth(textToWrite.charAt(i));
+		pct += charWidth/2 * pixToAngularPct;
+		
+		//calculate angle
+		let leftP = pointForIndex(pct-0.01);
+		let rightP = pointForIndex(pct+0.01);
+		let angle = atan2(leftP.y - rightP.y, leftP.x - rightP.x) + PI;
+		
+		push();
+			let p = pointForIndex(pct);
+			//apply angle
+			translate(p.x, p.y);
+			rotate(angle);
+			translate(-p.x, -p.y);
+		    fill(0,0,0);
             textSize(25);
-      text(textToWrite.charAt(i), p.x-charWidth/2, p.y);
-    pop();
-    
-    pct += charWidth/2 * pixToAngularPct;
-  }//for
+			text(textToWrite.charAt(i), p.x-charWidth/2, p.y);
+		pop();
+		
+		pct += charWidth/2 * pixToAngularPct;
+	}//for
 }
 
 function setImage(loadedImageFile) {
@@ -128,7 +128,7 @@ function setImage(loadedImageFile) {
 }
 
 function pointForIndex(pct) {
-  const NOISE_SCALE       = 1.5;
+	const NOISE_SCALE       = 1.5;
   let angle = pct * TWO_PI;
   let cosAngle = cos(angle);
   let sinAngle = sin(angle);
@@ -136,9 +136,9 @@ function pointForIndex(pct) {
   let noiseValue = noise(NOISE_SCALE * cosAngle + NOISE_SCALE, NOISE_SCALE * sinAngle + NOISE_SCALE, time);
   let radius = INNER_RADIUS + RADIUS_VARIATION * noiseValue;
   return {
-    x: radius * cosAngle + centerX,
-    y: radius * sinAngle + centerY
-  };
+		x: radius * cosAngle + centerX,
+		y: radius * sinAngle + centerY
+	};
 }
 
 function keyPressed() {
@@ -150,6 +150,7 @@ function keyPressed() {
     return;
   }else if(key === 'Enter')
   {
+      document.getElementById("loader").style.display = "block";
     sentiment = ml5.sentiment('movieReviews', modelReady);
     return;
   }else{
